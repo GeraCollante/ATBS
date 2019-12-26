30e333579ca5d6dfb8e12ef9975c0919df1a944f
GeraCollante <you@example.com>
2019-12-25 23:34:35 -0300

    Actualización

--- C5_DicAndStructData/inventory.py
+++ C5_DicAndStructData/inventory.py
@@ -26,3 +26,5 @@ inv = {'gold coin': 42, 'rope': 1}
 dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
 inv = addToInventory(inv, dragonLoot)
 displayInventory(inv)
+
+print('Aguante Messi')
